870f028a4cfc639a5095a0e7d06197befced3c8d
Jean-François Pérusse <jfperusse@gmail.com>
2023-06-23 10:58:59 -0400

    Cleaned-up comment.

--- src/aircodepath.ts
+++ src/aircodepath.ts
@@ -2,7 +2,7 @@ export class AirCodePath {
     collection: string;
     project: string;
 
-    // codea://ip:port/Codea/Collection/Project/...
+    // /Codea/Collection/Project/...
     constructor(path: string) {
         const parts = path.split('/');
 
